63e829bd3c6349842455fcd23678177ed45fd33d
Sarah Padovani <sarah.padovani@redspace.com>
2019-05-24 09:17:21 -0300

    initial commit

---
+++ home.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// TODO 5: reference the docs to customize the behaviour of the slider
+// https://github.com/ganlanyuan/tiny-slider
+
+const slider = tns({
+  container: '.carousel__track',
+  controlsPosition: 'bottom',
+  controlsText: ['', ''],
+  items: 1,
+  navPosition: 'bottom',
+  speed: 300,
+  // axis: 'vertical',
+  // items: 2,
+  // slideBy: 2,
+  // autoplay: true,
+  // mouseDrag: true,
+})
